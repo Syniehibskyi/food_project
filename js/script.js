271ee360ef975467e0e7073b2406fea149111197
Mykyta <sinegibskij@gmail.com>
2021-06-22 19:17:39 +0300

    modal added

--- js/script.js
+++ js/script.js
@@ -93,4 +93,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
   setClock('.timer', deadline);
 
+//  Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow ='hidden';
+    });
+  });
+
+   const closeModal = () => {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    document.body.style.overflow ='';
+  };
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        closeModal();
+    } 
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+    }
+  })
 });
